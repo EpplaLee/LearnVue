985db103fb26923a8a749a62d257b3f6223ae954
MagicConch <350043743@qq.com>
2017-01-27 16:33:17 +0800

    review

--- foundation/表单控件绑定/eg1.js
+++ foundation/表单控件绑定/eg1.js
@@ -9,9 +9,15 @@ var app = new Vue({
     selected1: "",
     selected2: [],
     selected3: "",
+    selected4: "",
     list:[
       { text: '我要', value: '你在我身旁'},
       { text: '我要', value: '看着你梳妆'}
+    ],
+    options:[
+      { value:"A", text:"三短一长选最长" },
+      { value:"B", text: "三长一短选最短" },
+      { value:"C", text: "两长两短就选C" } 
     ]
   }
 });
