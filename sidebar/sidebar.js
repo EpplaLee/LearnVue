230dafb4fa93efbb966a6cf1ccd5b809326c3e94
MagicConch <350043743@qq.com>
2017-04-10 16:54:30 +0800

    ..

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,12 +1,6 @@
 var sidebar = new Vue({
     el: '#body',
     data: {
-        isHidden: false
-    },
-    methods: {
-        toggleSidebar: function() {
-            if(this.isHidden) this.isHidden = false;
-            else this.isHidden = true;
-        }
+        show: true
     }
 });
